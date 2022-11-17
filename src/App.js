e1fe67cb55c90778ffd1697e62a45750d981f8fe
ibrahim-mohamed-rezk <ibrahimmo989800@gmail.com>
2022-11-17 05:53:12 -0500

    fix todo remove when reload

--- src/App.js
+++ src/App.js
@@ -4,13 +4,26 @@ import EditToDo from './component/EditToDo';
 import ToDoBody from './component/ToDoBody';
 
 function App() {
-  const [edit, setEdit] = useState(false)
-  const [toDoIndex, setToDoIndex] = useState()
-  const [todos, setTodos] = useState([])
+  const [edit, setEdit] = useState(false);
+  const [toDoIndex, setToDoIndex] = useState();
+  const [todos, setTodos] = useState([]);
+
+  if (localStorage.getItem("todos") == null) {
+    localStorage.setItem("todos", "");
+  }
+
   return (
     <div className="App">
-      {edit || <ToDoBody  edit={edit} setEdit={setEdit} todos = {todos} setTodos = {setTodos} setToDoIndex = {setToDoIndex} />}
-      {!edit || <EditToDo todos = {todos} setTodos = {setTodos} toDoIndex ={toDoIndex} setEdit = {setEdit}  />}
+      {edit || (
+        <ToDoBody
+          edit={edit}
+          setEdit={setEdit}
+          todos={todos}
+          setTodos={setTodos}
+          setToDoIndex={setToDoIndex}
+        />
+      )}
+      {!edit || <EditToDo toDoIndex={toDoIndex} setEdit={setEdit} />}
     </div>
   );
 }
--- src/component/EditToDo.js
+++ src/component/EditToDo.js
@@ -1,36 +1,38 @@
 import React, { useState } from 'react'
 
-const EditToDo = ({todos, setTodos, toDoIndex, setEdit}) => {
+const EditToDo = ({ toDoIndex, setEdit }) => {
+  const allTodos = localStorage.getItem("todos").split(",");
 
-    const [editInput, setEditInput] = useState('')
+  const [editInput, setEditInput] = useState("");
 
-    const EditToDo = ()=>{
-        todos[toDoIndex] = editInput
-        const newTodo = [...todos]
-        setTodos(newTodo)
-        setEdit(false)
-    }
+  const EditToDo = () => {
+    allTodos[toDoIndex] = editInput;
+    const newTodo = [...allTodos];
+    localStorage.setItem("todos", newTodo);
+    setEdit(false);
+  };
 
   return (
     <div>
-        <h1 className='title'>Edit ToDo</h1>
+      <h1 className="title">Edit ToDo</h1>
 
-        <div className='ToDoBody' >
-            <form onSubmit={EditToDo} className='toDoForm'>
-                <input 
-                    type='text' 
-                    className='addToDoInput' 
-                    value={editInput} 
-                    onChange={(e) =>{setEditInput(e.target.value)}} 
-                    placeholder='Edit TODO '
-                    maxLength='50' 
-                />
-                <button  className='addToDoBtn'>Edit</button>
-            </form>
-        </div>
-
+      <div className="ToDoBody">
+        <form onSubmit={EditToDo} className="toDoForm">
+          <input
+            type="text"
+            className="addToDoInput"
+            value={editInput}
+            onChange={(e) => {
+              setEditInput(e.target.value);
+            }}
+            placeholder="Edit TODO "
+            maxLength="50"
+          />
+          <button className="addToDoBtn">Edit</button>
+        </form>
+      </div>
     </div>
-  )
-}
+  );
+};
 
 export default EditToDo
